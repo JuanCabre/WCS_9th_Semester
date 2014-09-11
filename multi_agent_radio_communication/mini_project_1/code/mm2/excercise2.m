89cb759fe05aa18975ad4b6dffa402447fa28abb
Anderskars <anders.karstensen@gmail.com>
2014-09-11 16:03:35 +0200

    code update

--- multi_agent_radio_communication/mini_project_1/code/mm2/excercise2.m
+++ multi_agent_radio_communication/mini_project_1/code/mm2/excercise2.m
@@ -2,29 +2,7 @@
 clear all
 close all
 
-%% For Djam
-
-% make X number of uncorrelated channels
-nr_channels = 8;                % dont use more than 8, error on plotting due to color function.
-nr_realization = 1000;
-for k = 1:nr_channels
-    channel(k,:) = (randn(1,nr_realization) .* exp(-j*randn(1,nr_realization)));
-end
-
-c = get(gca,'colororder');  % get color vector
-c(8,:) = [1,1,1];           % get color vector
-
-% plot the envelope of the channels
-for k = 1:nr_channels
-plot(abs(channel(k,:)),'color',c(k,:)); hold on; grid on;
-end
-text(100,3.5,'test')
-title('Envelope, two rayleigh fading channels');
-xlabel('sample number');
-ylabel('amplitude');
-
-
-%% excercise 1.2 a
+% excercise 1.2 a
 
 % correlated channels
 p = 0;             % correlation coeffiecient
@@ -39,15 +17,38 @@ end
 
 % check correlation coefficient, can use both functions for same result,
 % corrcoeff best for matrix coefficient
-check_correlation = abs(corr(u',v'))          
-% abs(corrcoef(u',v'))
+check_correlation = abs(corr(u',v'));          
+abs(corrcoef(u',v'));
 
+corr_value = char(sprintf('Correlation coefficient %.3f',check_correlation));
 
-figure()
+figure()  % envelope of channels
 plot(abs(u),'color','r'); hold on; grid on;
 plot(abs(v),'color','b');
-text(100,3.5,'test')
+text(25,3,corr_value);
 title('Envelope, two rayleigh fading channels');
 xlabel('sample number');
 ylabel('amplitude');
 legend('channel 1','channel 2')
+
+
+% Make the CDF plot of the two channels
+u_db = 20*log10(abs(u));    % convert to dB
+v_db = 20*log10(abs(v));
+H1 = sort(u_db);            % sort for CDF
+H2 = sort(v_db);
+
+figure()
+Percent_Axis = linspace (0 ,100 , 1000);
+semilogy(H1,Percent_Axis,'b'); hold on; grid on;
+semilogy(H2,Percent_Axis,'r');
+xlabel('Power [dB]')
+ylabel('Log (%)')
+title('CDF data plot')
+legend('channel 1','channel 2')
+
+
+% excercise 1.2 b
+
+H = [u(1,1), u]
+
