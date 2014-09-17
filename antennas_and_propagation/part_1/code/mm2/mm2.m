bf565458cff3a7878e9078d224d194bbd0c2e568
Anderskars <anders.karstensen@gmail.com>
2014-09-17 13:29:48 +0200

    figure and stuff

--- antennas_and_propagation/part_1/code/mm2/mm2.m
+++ antennas_and_propagation/part_1/code/mm2/mm2.m
@@ -12,8 +12,8 @@ mu = 1.257*10^-6;
 l = lambda_range./60;
 a = lambda_range./200;
 k = 2*pi./lambda_range;
-R_loss = 4.42*10^-3;
-R_rad = 0.219;
+R_loss = 4.415*10^-3;
+R_rad = 0.21932;
 Z_0 = 50;
 
 %impedance does not change
@@ -29,7 +29,7 @@ end
      R_rad(i) = 80*(pi^2)*(l(i)/lambda_range(i))^2;
      R_loss(i) = l(i)/(2*pi*a(i)) * sqrt( (2*pi*freq_range(i)*mu) / (2*sigma) );
      X_in(i) = -120 * ( (log(l(i)/a(i))-1) / (tan(k(i)*l(i))));
-     Z_in(i) = (R_rad(i) + R_loss(i))+j*X_in(i);
+     Z_in(i) = (R_rad(i) + R_loss(i)) + j*X_in(i);
      Gamma(i) = (Z_in(i) - Z_0) / (Z_in(i) + Z_0); 
      SWR_2(i) = (1+abs(Gamma(i))) / (1-abs(Gamma(i)));
  end
@@ -40,4 +40,4 @@ end
  plot(freq_range,SWR_2);
  xlabel('Frequency (GHz)')
  ylabel('VSWR')
- legend('constant impedance','changing impedance');
+ legend('constant impedance (1GHz)','frequency dependant impedance');
