929243521776cab4d8280cbe5736c1d5d4d16cd2
Burakito <pfuentespazos@gmail.com>
2014-11-25 14:15:28 +0100

    biutifu

--- antennas_and_propagation/part_1/code/mm10/exercise_A_MM10.m
+++ antennas_and_propagation/part_1/code/mm10/exercise_A_MM10.m
@@ -16,7 +16,7 @@ clf
 f=2400;
 lambda=300/f;
 k=2*pi/lambda;
-N=0;   
+N=10;   
 a=10;   % what is this ? = Distance between the walls
 h1=10;  % height terminal 1
 h2=2;   % height terminal 2
@@ -58,10 +58,55 @@ grid on
 % How will you plot the time response (power versus delay)?
 
 
-% Impulse response
-figure()
+% % Impulse response
+% figure()
+dref = 100;
+ddis = sqrt(dref.^2+(h1-h2)^2);
+
+R1=sqrt(dref.^2+(h1-h2)^2);  % distance between antenna and side images;
+R2=sqrt(dref.^2+(h1+h2)^2);  % distance between ground image and side images;
+
+Floss1 = lambda./(4.*pi.*R1); 
+Floss2 = lambda./(4.*pi.*R2);
+
+E1 = Floss1.*exp(-j*k*R1);
+E2 = (Rg).*Floss2.*exp(-j*k*R2);
+
+dis = [R1;R2];
+amp = [E1;E2];
+
+if(N>0)
+for i=2:N+1
+%insert here the contribution from the various images and sum them to a total E;
+R1=sqrt(dref.^2+(h1-h2)^2+((i-1)*a)^2);  % distance between antenna and side images;
+R2=sqrt(dref.^2+(h1+h2)^2+((i-1)*a)^2);  % distance between ground image and side images;
+Floss1 = lambda./(4.*pi.*R1); 
+Floss2 = lambda./(4.*pi.*R2);
+
+E1 = 2*Floss1.*((Rw^(i-1))*exp(-j*k*R1));
+E2 = 2*(Rg).*Floss2.*((Rw^(i-1))*exp(-j*k*R2));
+
+dis = [dis [R1;R2]];
+amp = [amp [E1;E2]];
+
+end
+end
+
+delay = (dis - ddis)./(3e8);
+P=20*log10(abs(amp));
 
 
+delay_v = reshape(delay,[1 numel(delay)]);
+P = P - P(1,1);
+P_v = reshape(P,[1 numel(P)]);
+
+figure;
+stem(delay_v,P_v,'r','linewidth',2)
+xlabel('Delay (s)','FontSize',15);
+ylabel('Power (dB)','FontSize',15);
+title('Field strength vs delay','FontSize',15);
+% handle=legend('Field strength', 'Path loss');
+% set(handle,'FontSize',12);
 
 
 % % % % % % Impulse response in frequency
