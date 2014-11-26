2402bfb9231efe8db5cd2fbc84ab9ed9265eb948
DjSa <djamschid.safi@gmail.com>
2014-11-26 12:02:37 +0100

    MM3 again...

--- multi_agent_radio_communication/mini_project_1/code/mm3/Acm_PvsD.m
+++ multi_agent_radio_communication/mini_project_1/code/mm3/Acm_PvsD.m
@@ -12,7 +12,7 @@ figure
 h1=plot(d,P1);
 hold on
 h2=plot(d,P2);
-plot([1,30],[Pdc, Pdc],'-')
+plot([1,30],[Pdc, Pdc],'--')
 xlabel('Distance d [m]')
 ylabel('Power at distance d')
 legend([h1, h2],'Power for one antenna','P for two antennas')
@@ -22,7 +22,7 @@ figure
 h1=plot(area,P1);
 hold on
 h2=plot(area,P2);
-plot([1,900],[Pdc, Pdc],'-')
+plot([1,900],[Pdc, Pdc],'--')
 xlabel('Area [m^2]')
 ylabel('Power')
 legend([h1, h2],'Power for one antenna','P for two antennas')
